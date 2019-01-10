d3fd784972ed74367075b748d5923ef43c1263ee
catalinvr <Catalin-Liviu.Vrinceanu@redbull.com>
2019-01-10 10:02:07 +0100

    created first controller

---
+++ src/main/java/com/example/controller/RootController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class RootController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
